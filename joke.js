f31f0b0c33ed31e687931674eb686414a31501d5
Dev-Anky07 <anky007cj@gmail.com>
2023-11-09 12:35:24 +0530

    Added relevant Functionality wrt archiving yt

---
+++ joke.js
@@ -0,0 +1,64 @@
+import dotenv from "dotenv";
+import { Client, GatewayIntentBits } from "discord.js";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const openai = new OpenAIApi(new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+);
+
+const conversationCache = {};
+
+client.on("messageCreate", async function (message) {
+  if (message.author.bot) return;
+
+  const conversationId = message.channel.id;
+  const conversation = conversationCache[conversationId];
+
+  // Check if the message mentions the bot
+  const botMentioned = message.mentions.has(client.user);
+
+  try {
+    let messages = [
+      //{ role: "system", content: "You are a friend cum assistant who answers succinctly. You follow a particlar format" },
+      { role: "system", content: "You are like a friend who gives sarcastic, leg pulling responses but all in good fun" },
+    ];
+
+    if (conversation) {
+      messages = [...messages, ...conversation];
+    }
+
+    messages.push({ role: "user", content: message.content });
+
+    if (botMentioned) {
+      const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages,
+       // temperature: 0.1, // Set the temperature parameter (adjust as desired)
+       // maxTokens: 1000, // Set the maxTokens parameter (adjust as desired)
+      });
+
+      const content = response.data.choices[0].message;
+      await message.reply(content);
+    }
+
+    // Cache the conversation history
+    conversationCache[conversationId] = messages;
+
+  } catch (err) {
+    console.error(err);
+    await message.reply("I'm sorry, I'm not smart enogh to tell you that.");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
